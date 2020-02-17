9fecac684a1a73cc55fcec75ed70984ce3e29f6b
bedamunk <60632339+bedamunk@users.noreply.github.com>
2020-02-17 15:00:57 -0600

    Very much in progress but closing in on finishing up pretty soon here.

--- Account/src/account/UserAccounts.java
+++ Account/src/account/UserAccounts.java
@@ -2,6 +2,7 @@ package account;
 import account.Account;
 import java.util.*;
 import java.text.*;
+
 /**
  * @author Red Team
  * Sam Allen
@@ -11,35 +12,60 @@ import java.text.*;
  * Bethanie Munrkes
  */
 public class UserAccounts {
-	// class defines an array of 8 accounts as an instance variable
-	
-	/** default constructor: write a loop that creates 8 accounts with ids 0 through 7
-	 *  with initial balance of $50 for each account.  Store these accounts in the array.
-	 */
-     // When the program runs, it asks the user to enter a specific id.
-	
-	// When the user enters a correct id, the system displays a menu
-	/** Main menu
-	 * 1: check balance
-	 * 2: withdraw
-	 * 3: deposit
-	 * 4: exit
-	 * Enter a choice: 
-	 * (1)The balance is 50.0
-	 * (2)Enter an amount to withdraw: 
-	 * (2)The balance is 25.0
-	 * (3)Enter an amount to deposit: 
-	 * (3)The balance is 35.0
-	 * (4)Enter an id:
-	 **/
-	 // Option 4 exits the main menu
-	 // So,  if option 4 is chosen, the system will prompt the user again to enter 
-	 // another id.
-	 // The system will keep running indefinitely
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// class defines an array of 8 accounts as an instance variable
+		Account[] userAccountsArray = new Account[8];
+		for (int i = 0; i < userAccountsArray.length; i++) {
+			userAccountsArray[i] = new Account(i, 50.0);
+		}
+		boolean userIdInput = false;
+		//******************************************** 373
+		//********************************************
+		// When the user enters a correct id, the system displays a menu
+		/** Main menu
+		 * 1: check balance
+		 * 2: withdraw
+		 * 3: deposit
+		 * 4: exit
+		 * Enter a choice: 
+		 * (1)The balance is 50.0
+		 * (2)Enter an amount to withdraw: 
+		 * (2)The balance is 25.0
+		 * (3)Enter an amount to deposit: 
+		 * (3)The balance is 35.0
+		 * (4)Enter an id:
+		 **/
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter an id: ");
+		int userInputId = input.nextInt();
+		while (userInputId < 0 || userInputId > 7) {
+			System.out.println("Please enter an existing account id: ");
+			userInputId = input.nextInt();			
+		}
+		// Create scanner for first set of menu options
+		Scanner firstMenu = new Scanner(System.in);
+		System.out.println("Main menu");
+		System.out.println("1: check balance");
+		System.out.println("2: withdraw");
+		System.out.println("3: ");
+		System.out.println("");
 
-	}
-
+		if (userInputId == 1) {
+			double giveBalance = userAccountsArray[userInputId].getBal();
+			System.out.println(giveBalance);
+		} else if (userInputId == 2) {
+					
+			} else if (userInputId == 3) {
+					
+				// Option 4 exits the main menu
+				// So,  if option 4 is chosen, the system will prompt the user again to enter 
+				// another id.
+				// The system will keep running indefinitely
+			} else if (userInputId == 4) {
+				
+			}
+		
+		
+}
 }
