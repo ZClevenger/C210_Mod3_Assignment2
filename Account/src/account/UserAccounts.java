289244db579d099a8f9fcf2b68f550b8d522df3c
bedamunk <60632339+bedamunk@users.noreply.github.com>
2020-02-16 23:54:51 -0600

    updated basic skeleton for UserAccounts

---
+++ Account/src/account/UserAccounts.java
@@ -0,0 +1,45 @@
+package account;
+import account.Account;
+import java.util.*;
+import java.text.*;
+/**
+ * @author Red Team
+ * Sam Allen
+ * Zachary Clevenger
+ * Amber Ferrebee
+ * Angela Johnson
+ * Bethanie Munrkes
+ */
+public class UserAccounts {
+	// class defines an array of 8 accounts as an instance variable
+	
+	/** default constructor: write a loop that creates 8 accounts with ids 0 through 7
+	 *  with initial balance of $50 for each account.  Store these accounts in the array.
+	 */
+     // When the program runs, it asks the user to enter a specific id.
+	
+	// When the user enters a correct id, the system displays a menu
+	/** Main menu
+	 * 1: check balance
+	 * 2: withdraw
+	 * 3: deposit
+	 * 4: exit
+	 * Enter a choice: 
+	 * (1)The balance is 50.0
+	 * (2)Enter an amount to withdraw: 
+	 * (2)The balance is 25.0
+	 * (3)Enter an amount to deposit: 
+	 * (3)The balance is 35.0
+	 * (4)Enter an id:
+	 **/
+	 // Option 4 exits the main menu
+	 // So,  if option 4 is chosen, the system will prompt the user again to enter 
+	 // another id.
+	 // The system will keep running indefinitely
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
