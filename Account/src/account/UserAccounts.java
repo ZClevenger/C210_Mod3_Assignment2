1ad1c58efe4360327ee146e42fe28dc0b66f67b5
bedamunk <60632339+bedamunk@users.noreply.github.com>
2020-02-17 16:39:50 -0600

    UserAccounts ready for testing

--- Account/src/account/UserAccounts.java
+++ Account/src/account/UserAccounts.java
@@ -1,7 +1,7 @@
 package account;
 import account.Account;
 import java.util.*;
-import java.text.*;
+
 
 /**
  * @author Red Team
@@ -19,9 +19,8 @@ public class UserAccounts {
 		for (int i = 0; i < userAccountsArray.length; i++) {
 			userAccountsArray[i] = new Account(i, 50.0);
 		}
-		boolean userIdInput = false;
-		//******************************************** 373
-		//********************************************
+		//boolean userIdInput = false;
+
 		// When the user enters a correct id, the system displays a menu
 		/** Main menu
 		 * 1: check balance
@@ -36,6 +35,8 @@ public class UserAccounts {
 		 * (3)The balance is 35.0
 		 * (4)Enter an id:
 		 **/
+		boolean startOver = false;
+		do {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter an id: ");
 		int userInputId = input.nextInt();
@@ -50,22 +51,44 @@ public class UserAccounts {
 		System.out.println("2: withdraw");
 		System.out.println("3: deposit");
 		System.out.println("4: exit");
-		int choice = menuChoice.nextInt();
-		if (choice == 1) {
-			double giveBalance = userAccountsArray[userInputId].getBal();
-			System.out.println(giveBalance);
-		} else if (choice == 2) {
-					
-			} else if (choice == 3) {
-					
-				// Option 4 exits the main menu
-				// So,  if option 4 is chosen, the system will prompt the user again to enter 
-				// another id.
-				// The system will keep running indefinitely
-			} else if (choice == 4) {
-				
-			}
+		int choice;
+		do {
+			choice = menuChoice.nextInt();
 		
+		switch (choice) {
+		case 1:	double giveBalance = userAccountsArray[userInputId].getBal();
+				System.out.println("The balance is " + giveBalance); break;
+		case 2: Scanner amtToWithdraw = new Scanner(System.in);
+				System.out.println("Enter an amount to withdraw: ");
+				double withdrawnAmount = amtToWithdraw.nextDouble();
+				giveBalance = userAccountsArray[userInputId].getBal() - withdrawnAmount;
+				userAccountsArray[userInputId].setBal(giveBalance); 
+				// test ************** REMOVE BEFORE SUBMISSION
+				//giveBalance = userAccountsArray[userInputId].getBal();
+				//System.out.println("The balance is " + giveBalance);
+				// end of test ******* END REMOVE BEFORE SUBMISSION
+				break;
+		case 3: Scanner amtToDeposit = new Scanner(System.in);
+				System.out.println("Enter an amount to deposit: ");
+				double depositedAmount = amtToDeposit.nextDouble();
+				giveBalance = userAccountsArray[userInputId].getBal() + depositedAmount;
+				userAccountsArray[userInputId].setBal(giveBalance); 
+				// test ************** REMOVE BEFORE SUBMISSION
+				//giveBalance = userAccountsArray[userInputId].getBal();
+				//System.out.println("The balance is " + giveBalance);
+				// end of test ******* END REMOVE BEFORE SUBMISSION
+				break;
+		case 4: startOver = true;
+		}
+				System.out.println("Main menu");
+				System.out.println("1: check balance");
+				System.out.println("2: withdraw");
+				System.out.println("3: deposit");
+				System.out.println("4: exit");
+
+			} while(choice != 4);
 		
+
+} while (startOver = true);
 }
 }
