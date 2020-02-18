cc3b3086d4e4bf809b8cbb5b47ce0dcf2ab043d2
bedamunk <60632339+bedamunk@users.noreply.github.com>
2020-02-18 00:21:20 -0600

    Update UserAccounts.java

--- Account/src/account/UserAccounts.java
+++ Account/src/account/UserAccounts.java
@@ -16,31 +16,22 @@ public class UserAccounts {
 	public static void main(String[] args) {
 		// class defines an array of 8 accounts as an instance variable
 		Account[] userAccountsArray = new Account[8];
-		for (int i = 0; i < userAccountsArray.length; i++) {
-			userAccountsArray[i] = new Account(i, 50.0);
+		
+		/*write a loop that creates 8 accounts with ids 0 through 7 and 
+		 *initial balance of $50 for each account.  Store these accounts
+		 *in the array.
+		 */ 
+		 for (int i = 0; i < userAccountsArray.length; i++) {
+			userAccountsArray[i] = new Account(i, 50.00);
 		}
-		//boolean userIdInput = false;
-
-		// When the user enters a correct id, the system displays a menu
-		/** Main menu
-		 * 1: check balance
-		 * 2: withdraw
-		 * 3: deposit
-		 * 4: exit
-		 * Enter a choice: 
-		 * (1)The balance is 50.0
-		 * (2)Enter an amount to withdraw: 
-		 * (2)The balance is 25.0
-		 * (3)Enter an amount to deposit: 
-		 * (3)The balance is 35.0
-		 * (4)Enter an id:
-		 **/
+		
 		boolean startOver = false;
 		do {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter an id: ");
 		int userInputId = input.nextInt();
-		while (userInputId < 0 || userInputId > 7) {
+		// When the user enters a correct id, the system displays a menu
+		while (userInputId < 0 || userInputId > userAccountsArray.length - 1) {
 			System.out.println("Please enter an existing account id: ");
 			userInputId = input.nextInt();			
 		}
@@ -48,44 +39,33 @@ public class UserAccounts {
 		do {
 		// Create scanner for menu options
 		Scanner menuChoice = new Scanner(System.in);
+		choice = menuChoice.nextInt();
 		System.out.println("Main menu");
-		System.out.println("1: check balance");
-		System.out.println("2: withdraw");
-		System.out.println("3: deposit");
-		System.out.println("4: exit");
-		
-			choice = menuChoice.nextInt();
-		
+		System.out.println("1: Check Balance");
+		System.out.println("2: Withdraw");
+		System.out.println("3: Deposit");
+		System.out.println("4: Exit");
+	
 		switch (choice) {
+		// use getBal to display balance
 		case 1:	double giveBalance = userAccountsArray[userInputId].getBal();
-				System.out.println("The balance is " + giveBalance); break;
+				System.out.printf("The balance is $%.2f.\n", giveBalance); break;
+		// use setBal to update with withdrawal amount
 		case 2: Scanner amtToWithdraw = new Scanner(System.in);
 				System.out.println("Enter an amount to withdraw: ");
 				double withdrawnAmount = amtToWithdraw.nextDouble();
 				giveBalance = userAccountsArray[userInputId].getBal() - withdrawnAmount;
-				userAccountsArray[userInputId].setBal(giveBalance); 
-				// test ************** REMOVE BEFORE SUBMISSION
-				//giveBalance = userAccountsArray[userInputId].getBal();
-				//System.out.println("The balance is " + giveBalance);
-				// end of test ******* END REMOVE BEFORE SUBMISSION
-				break;
+				userAccountsArray[userInputId].setBal(giveBalance);break;
+		// use setBal to update balance with deposit amount
 		case 3: Scanner amtToDeposit = new Scanner(System.in);
 				System.out.println("Enter an amount to deposit: ");
 				double depositedAmount = amtToDeposit.nextDouble();
 				giveBalance = userAccountsArray[userInputId].getBal() + depositedAmount;
-				userAccountsArray[userInputId].setBal(giveBalance); 
-				// test ************** REMOVE BEFORE SUBMISSION
-				//giveBalance = userAccountsArray[userInputId].getBal();
-				//System.out.println("The balance is " + giveBalance);
-				// end of test ******* END REMOVE BEFORE SUBMISSION
-				break;
+				userAccountsArray[userInputId].setBal(giveBalance);	break;
+		// exit menu and return to request account id
 		case 4: startOver = true;
-		}
-
-
-			} while(choice != 4);
-		
-
-} while (startOver = true);
-}
+						}
+				} while(choice != 4);
+		} while (startOver);
+	}
 }
